de61f42952f68e21c8164bffed87655831bd5d84
slavik <barthez.slavik@gmail.com>
2016-04-08 06:48:50 +0300

    Dynamic selects

--- assets/app.js
+++ assets/app.js
@@ -1,31 +1,48 @@
 angular.module('app', [])
   .controller('mainController', function($scope) {
-    $scope.add = function() {
-      $("form").show();
+    $scope.list = [];
+
+    $scope.update = function() {
+      var name = $scope.current_type;
+      $scope.current = $scope[name.id];
     }
+
+    $scope.add = function() {
+      $scope.list.push($scope.data);
+      delete($scope.data)
+    }
+
+    $scope.item_types = [
+      { id: 'gadgets', name: "Gadgets" },
+      { id: 'fixed_discounts', name: "Fixed discount" },
+      { id: 'percent_discounts', name: "Percent discount" },
+      { id: 'fees', name: "Fees" }
+    ];
 
     $scope.gadgets = [
       {
+        id: 1,
         name: "Gadget_221",
         cost: 100,
       },
       {
+        id: 2,
         name: "Gadget_113",
         cost: 50,
       },
     ];
 
-    $scope.local_discounts = [
+    $scope.fixed_discounts = [
       {
         name: "Old Client Discount",
         cost: 20,
       },
     ];
 
-    $scope.global_discounts = [
+    $scope.percent_discounts = [
       {
         name: "Student Discount",
-        cost: "20%",
+        cost: "20",
       },
     ];
 
